16ececc318659df74719d7403ae1610059ad18a5
arantxagonzaleziturra@gmail.com <arantxagonzaleziturra@gmail.com>
2025-12-11 21:43:35 -0300

    js grupo

---
+++ assets/js/grupo 2/grupo2.js
@@ -0,0 +1,12 @@
+let nombre = "Emilio";
+let salarioBase = 800000;       // sueldo mensual
+let añosDeServicio = 7;         // años trabajados
+let ventasAnuales = 9500;       // ventas del año
+if (añosDeServicio < 5) {
+factorAntiguedad = 0.05;
+} else if (añosDeServicio >= 5 && añosDeServicio <= 10) {
+factorAntiguedad = 0.10;
+} else {
+factorAntiguedad = 0.15;
+}
+if ()
