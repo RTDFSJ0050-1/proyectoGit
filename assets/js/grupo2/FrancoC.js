f4599bf2fb40debc37e7eb8c04e7bf47436d4efa
FrancoCanD <f.cancinodiaz@gmail.com>
2025-12-17 03:31:27 -0300

    Creación archivo FrancoC.js, se agregan algunas funciones de JavaScript

---
+++ assets/js/grupo2/FrancoC.js
@@ -0,0 +1,27 @@
+function mostrarprompt() {
+    let numero1 = Number(prompt("Ingresa el primer número:"));
+    let numero2 = Number(prompt("Ingresa el segundo número"));
+
+    let suma = numero1 + numero2;
+    let resta = numero1 - numero2;
+    let multiplicacion = numero1 * numero2;
+    let division = numero1 / numero2; 
+
+    alert('La suma es: ' + suma);
+    alert('La resta es: ' + resta);
+    alert('La multiplicación es: ' +multiplicacion);
+    alert('La división es: ' + division);
+  }function mostrarprompt() {
+      let numero1 = Number(prompt("Ingresa el primer número:"));
+      let numero2 = Number(prompt("Ingresa el segundo número"));
+
+      let suma = numero1 + numero2;
+      let resta = numero1 - numero2;
+      let multiplicacion = numero1 * numero2;
+      let division = numero1 / numero2; 
+
+      alert('La suma es: ' + suma);
+      alert('La resta es: ' + resta);
+      alert('La multiplicación es: ' +multiplicacion);
+      alert('La división es: ' + division);
+    }
